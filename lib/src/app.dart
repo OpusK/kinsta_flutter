3243fa5eb0c775e688b7fc1ae7d8683b87dac156
Kei <opusk44@gmail.com>
2022-11-06 16:58:52 +0900

    add bottom navbar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:instaclone_flutter/src/components/image_data.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Container(),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+              icon: ImageData(IconsPath.homeOff),
+              activeIcon: ImageData(IconsPath.homeOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconsPath.searchOff),
+              activeIcon: ImageData(IconsPath.searchOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconsPath.uploadIcon),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageData(IconsPath.activeOff),
+              activeIcon: ImageData(IconsPath.activeOn),
+              label: 'home',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                width: 30,
+                height: 30,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey,
+                ),
+              ),
+              label: 'home',
+            ),
+          ],
+        ),
+      ),
+      onWillPop: () async {
+        return false;
+      },
+    );
   }
 }
